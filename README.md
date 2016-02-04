49ef8cd0e66578dc3d1b20b7d949a516a79332c1
wednesday_developer <wednesday.developer@gmail.com>
2016-02-04 11:42:13 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+ここを参考にしました。
+
+http://qiita.com/nownabe/items/2d8b92d95186c3941de0
---
+++ index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class CommentBox extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+    render() {
+    return(
+      <div className='commentBox'>
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('container')
+);
