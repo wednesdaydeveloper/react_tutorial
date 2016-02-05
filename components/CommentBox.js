8d59294d04896bb3ed8e154a3202c2e27c0644cc
wednesday_developer <wednesday.developer@gmail.com>
2016-02-05 22:21:24 +0000

    ｓししあしあｇしあげ仕上げ

--- components/CommentBox.js
+++ components/CommentBox.js
@@ -1,18 +1,55 @@
 import React from 'react'
 import CommentList from './CommentList'
 import CommentForm from './CommentForm'
+import $ from 'jquery'
 
 export default class CommentBox extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      data: []
+    };
+  }
+
+  loadCommentsFromServer() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: (data) => { this.setState({data: data}); },
+      error: (xhr, status, err) => {
+        console.error(this.props.url, status, err.toString());
+      }
+    });
+  }
+
+  handleCommentSubmit(comment) {
+    var comments = this.state.data;
+    var newComments = comments.concat([comment]);
+    this.setState({data: newComments});
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: comment,
+      success: (data) => { this.setState({data: data}); },
+      error: (xhr, status, err) => {
+        console.error(this.props.url, status, err.toString());
+      }
+    });
+  }
+
+  componentDidMount() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer.bind(this), this.props.pollInterval);
   }
 
   render() {
     return(
       <div className='commentBox'>
         <h2>Comments</h2>
-        <CommentList data={this.props.data} />
-        <CommentForm />
+        <CommentList data={this.state.data} />
+        <CommentForm onCommentSubmit={this.handleCommentSubmit.bind(this)} />
       </div>
     );
   }
--- index.js
+++ index.js
@@ -2,12 +2,7 @@ import ReactDOM from 'react-dom';
 import React from 'react'
 import CommentBox from './components/CommentBox'
 
-var data = [
-  {author: 'Pete Hunt', text: 'This is one comment'},
-  {author: 'Jordan Walke', text: 'This is *another* comment2'}
-];
-
 ReactDOM.render(
-  <CommentBox data={data} />,
+  <CommentBox url="comments.json" pollInterval={2000} />,
   document.getElementById('container')
 );
